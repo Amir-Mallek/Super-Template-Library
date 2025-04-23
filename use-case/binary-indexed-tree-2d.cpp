85550d056c67f304900f9ba2fe3111b72751bbab
ademby <adem.benyarou10@gmail.com>
2025-04-23 23:49:31 +0100

    BIT variations

--- use-case/binary-indexed-tree-2d.cpp
+++ use-case/binary-indexed-tree-2d.cpp
@@ -1,11 +1,22 @@
-# Binary Indexed Tree
+/**
+ * BIT 2D range queries, point updates
+ * Link : https://lightoj.com/problem/points-in-rectangle
+ */
 
-Use it for a short, simple & efficient code. (Always 1-indexed)
+#include <bits/stdc++.h>
+using namespace std;
 
-Only for reversible operations.
+#define rep(i, a, b) for (int i = a; i < (b); ++i)
+typedef long long ll;
+typedef vector<int> vi;
 
-## Classic BIT
-```cpp
+/**
+ * Classical Binary 1-Indexed Tree
+ * Methods are 0-indexed
+ * Notes :
+ * - BIT is halfway between a normal array and a prefix sum array
+ * - 1-indexing uses LSB(x) = (x & -x)
+ */
 template <class T>
 struct BIT
 {
@@ -38,10 +49,15 @@ struct BIT
         return query(r) - query(l - 1);
     }
 };
-```
 
-## Range Update & Range Query
-```cpp
+/**
+ * Range Query Range Update Binary 1-Indexed Tree
+ * Methods are 0-indexed
+ * Notes :
+ * - it uses the classic BIT as a blackbox
+ * - the used logic has a very nice visual explanation
+ * - beware of overflows on multiplications --> you may have to use Segment Tree
+ */
 template <class T>
 struct RangeBIT
 {
@@ -53,7 +69,7 @@ struct RangeBIT
         rep(i, 0, n)
             add(i, i, a[i]);
     }
-    void __add_till_infinity(int r, T val) /* '+val' to [r, +inf) */ 
+    void __add_till_infinity(int r, T val) /* '+val' to [r, +inf) */ /*private*/
     {
         B1.add(r, val);
         B2.add(r, (r - 1) * (-val));
@@ -72,10 +88,10 @@ struct RangeBIT
         return query(r) - query(l - 1);
     }
 };
-```
 
-## 2-Dimensional BIT
-```cpp
+/**
+ * 2D Binary 1-Indexed Tree
+ */
 template <class T>
 struct BIT2D
 {
@@ -109,22 +125,57 @@ struct BIT2D
     }
     T query(int x1, int y1, int x2, int y2) /* (x1,y1): lower left corner */
     {
-        return query(x2, y2) + query(x1-1, y1-1) - query(x1-1, y2) - query(x2, y1-1);
+        return query(x2, y2) + query(x1 - 1, y1 - 1) - query(x1 - 1, y2) - query(x2, y1 - 1);
     }
 };
-```
 
-## Linear Construction
-```cpp
-    BIT(vector<T> &a) : BIT(a.size())
+int counter = 0;
+void solve()
+{
+    cout << "Case " << (++counter) << ":\n";
+    int q;
+    cin >> q;
+    /*
+    //testing RangeBIT
+    RangeBIT<ll> a(n);
+    rep(i,0,n)
+        cout << a.query(i,i) << ' ';
+    cout << endl;
+    a.add(2,4,3);
+    rep(i,0,n)
+        cout << a.query(i,i) << ' ';
+    cout << endl;
+    // Works well
+    */
+    BIT2D<int> b(1001, 1001);
+    while (q--)
     {
-        rep(i, 0, n){
-            int idx = i+1;
-            t[idx] += a[i];
-            int nxt = idx + (idx & -idx);
-            if(nxt <= n)
-                t[nxt] += t[idx];
+        int c;
+        cin >> c;
+        if (c)
+        {
+            int x1, y1, x2, y2;
+            cin >> x1 >> y1 >> x2 >> y2;
+            cout << b.query(x1, y1, x2, y2) << '\n';
+        }
+        else
+        {
+            int x, y;
+            cin >> x >> y;
+            /*if point exist*/
+            if (b.query(x, y, x, y) == 0)
+                b.add(x, y, 1);
+            // cout << b.query(x,y) << "*\n";
         }
     }
+}
 
-```
+int main()
+{
+    // Preprocessing here please...
+    int t = 1;
+    cin >> t; // press `Tab` to go to solve
+    while (t--)
+        solve();
+    return 0;
+}
